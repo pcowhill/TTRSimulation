c9b42273672aee6482cdf5f53904f40798752d13
Houseknecht, Jessica Taylor <jhouseknecht6@gatech.edu>
2022-03-20 18:40:44 -0400

    Update InitializeTrainCardDeck.m
    Added a reference to the game rules

--- Simulation/SimulationSetup/InitializeTrainCardDeck.m
+++ Simulation/SimulationSetup/InitializeTrainCardDeck.m
@@ -4,11 +4,9 @@
 % constructs the initial state of train card pile (prior to shuffling and
 % dealing to players).
 
-% Jessica TODO: Need to cite:
-% https://teams.microsoft.com/_#/pdf/viewer/teams/https:~2F~2Fgtvault.sharepoint.com~2Fsites~2FModelingandSimulationTeam1~2FShared%20Documents~2FGeneral~2FTicket%20To%20Ride%20Resources~2FTicketToRideRules.pdf?threadId=19:umNZGR5vjjedPKaD1XMbJpupBGgasRLyEct9H7qcltM1@thread.tacv2&baseUrl=https:~2F~2Fgtvault.sharepoint.com~2Fsites~2FModelingandSimulationTeam1&fileId=00994582-f912-4be8-ac38-39748c218ed8&ctx=files&rootContext=items_view&viewerAction=view
-% for a list of the number of each type of card
-
 % These are the colors and respective number of cards from the North America board game.
+% Reference:
+% Ticket to Ride Rules. (2015). Days of Wonder. 
 NUM_PURPLE = int8(12);
 NUM_WHITE = int8(12);
 NUM_BLUE = int8(12);
