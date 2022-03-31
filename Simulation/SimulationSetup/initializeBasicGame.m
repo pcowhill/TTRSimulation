11c5303956c749e42dca44631db0db1f06c00566
Patrick Cowhill <pcowhill3@gatech.edu>
2022-03-31 18:52:03 -0400

    Added new mlapp layout, added ability to select players, rules, and random seed

--- Simulation/SimulationSetup/initializeBasicGame.m
+++ Simulation/SimulationSetup/initializeBasicGame.m
@@ -2,6 +2,17 @@ function game = initializeBasicGame(args)
 %INITIALIZEBASICGAME Initialize a basic Game
     
     nPlayers = args{1};
+    % Second argument is number of iterations, implementation is in another
+    % branch at the time of writting.
+    playerStrategies = args{3};
+    ruleset = args{4};
+    randomSeedSelection = args{5};
+
+    if string(randomSeedSelection) == "Shuffle"
+        rng('shuffle');
+    elseif string(randomSeedSelection) == "Consistent"
+        rng('default');
+    end
 
     BaseBoard.initializeBoard;
     BaseBoard.InitializeTrainCardDeck;
@@ -9,10 +20,22 @@ function game = initializeBasicGame(args)
 
     players = DummyPlayer.empty;
     for ix=1:nPlayers
-        players(ix) = DummyPlayer(ix);
+        if string(playerStrategies{ix}) == "Dummy Player"
+            players(ix) = DummyPlayer(ix);
+        else
+            error("The following player strategy is not defined in " + ...
+                  "initializeBasicGame: " + string(playerStrategies{ix}));
+        end
     end
 
-    game = Game(BOARD, players, DefaultRules(), TRAINS_DECK, DESTINATIONS_DECK);
+    if string(ruleset) == "Default Rules"
+        rules = DefaultRules();
+    else
+        error("The following ruleset is not defined in " + ...
+              "initializeBasicGame: " + string(ruleset));
+    end
+
+    game = Game(BOARD, players, rules, TRAINS_DECK, DESTINATIONS_DECK);
 
 
 end
