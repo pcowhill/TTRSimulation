d88e222a5b1dd0d5a0578efc77b4bb9a9ad12c82
Patrick Cowhill <pcowhill3@gatech.edu>
2022-03-31 20:04:30 -0400

    Removing RNG logic, to be added in Jessica's later PR

--- Simulation/SimulationSetup/initializeBasicGame.m
+++ Simulation/SimulationSetup/initializeBasicGame.m
@@ -2,17 +2,8 @@ function game = initializeBasicGame(args)
 %INITIALIZEBASICGAME Initialize a basic Game
     
     nPlayers = args{1};
-    % Second argument is number of iterations, implementation is in another
-    % branch at the time of writting.
     playerStrategies = args{3};
     ruleset = args{4};
-    randomSeedSelection = args{5};
-
-    if string(randomSeedSelection) == "Shuffle"
-        rng('shuffle');
-    elseif string(randomSeedSelection) == "Consistent"
-        rng('default');
-    end
 
     BaseBoard.initializeBoard;
     BaseBoard.InitializeTrainCardDeck;
