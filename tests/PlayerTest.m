63b0e627136aa0bad80e4de904a1100ed5df8ba7
Patrick Cowhill <pcowhill3@gatech.edu>
2022-04-21 21:58:51 -0400

    Fixing unit tests

--- tests/PlayerTest.m
+++ tests/PlayerTest.m
@@ -21,13 +21,13 @@ classdef PlayerTest < matlab.unittest.TestCase
             % Checks that actions are carried out correctly
             board = Board(Route(Location.Atlanta, Location.Charleston, Color.blue, 4));
             trainsDeckBlue = TrainsDeck(int8(0),int8(0),int8(100),int8(0),int8(0),int8(0),int8(0),int8(0),int8(0));
-            trainsDeckBlue.init(1,1,1);
+            trainsDeckBlue.init(1,1,1,RandStream('mt19937ar'));
             trainsDeckMulti = TrainsDeck(int8(0),int8(0),int8(0),int8(0),int8(0),int8(0),int8(0),int8(0),int8(100));
-            trainsDeckMulti.init(1,1,1);
+            trainsDeckMulti.init(1,1,1,RandStream('mt19937ar'));
             destinationsDeck = DestinationsDeck(DestinationTicketCard("Atlanta","Charleston",10),...
                 DestinationTicketCard("Atlanta","Charleston",10),...
                 DestinationTicketCard("Atlanta","Charleston",10));
-            destinationsDeck.init();
+            destinationsDeck.init(RandStream('mt19937ar'));
             rules = PlayerStubs.RulesStub();
             player = PlayerStubs.PlayerStub(1);
             rules.action=1;
@@ -36,31 +36,31 @@ classdef PlayerTest < matlab.unittest.TestCase
                 delete(logFileName);
             end
             logger = log4m.getLogger(logFileName);
-            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger);
+            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger, RandStream('mt19937ar'));
             obj.verifyEqual(player.trainCardsHand,[TrainCard("blue")], "Draw Card");
             for ix=1:4
-                player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger);
+                player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger, RandStream('mt19937ar'));
             end
             obj.verifyEqual(player.trainCardsHand,repmat(TrainCard("blue"),1,5), "Draw 5 Cards");
 
             rules.action=0;
-            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger);
+            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger, RandStream('mt19937ar'));
             obj.verifyEqual(player.trainCardsHand,[TrainCard("blue")], "Claim route with blue");
             obj.verifyEqual(player.victoryPoints,5,"Claim route with blue");
 
             rules.action=1;
-            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger);
-            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger);
+            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger, RandStream('mt19937ar'));
+            player.takeTurn(rules,board,trainsDeckBlue,destinationsDeck, logger, RandStream('mt19937ar'));
             rules.action=3;
-            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger);
-            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger);
+            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger, RandStream('mt19937ar'));
+            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger, RandStream('mt19937ar'));
             rules.action=0;
-            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger);
+            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger, RandStream('mt19937ar'));
             obj.verifyEqual(player.trainCardsHand,[TrainCard("multicolored")], "Claim route with multicolored");
             obj.verifyEqual(player.victoryPoints,10,"Claim route with multicolored");
 
             rules.action=2;
-            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger);
+            player.takeTurn(rules,board,trainsDeckMulti,destinationsDeck, logger, RandStream('mt19937ar'));
             obj.verifyEqual(player.destinationCardsHand,repmat(DestinationTicketCard("Atlanta","Charleston",10),1,2), "Draw destination tickets");
 
  
--- tests/TrainsDeckTest.m
+++ tests/TrainsDeckTest.m
@@ -12,7 +12,7 @@ classdef TrainsDeckTest  < matlab.unittest.TestCase
     methods(Test)
         function initTest(obj)
             trainsDeckBlue = TrainsDeck(int8(0),int8(0),int8(100),int8(0),int8(0),int8(0),int8(0),int8(0),int8(0));
-            starting = trainsDeckBlue.init(10,3,2);
+            starting = trainsDeckBlue.init(10,3,2,RandStream('mt19937ar'));
             obj.verifyEqual(length(starting),10);
             obj.verifyEqual(length(trainsDeckBlue.drawPile),87);
             obj.verifyEqual(length(trainsDeckBlue.faceUpCards),3);
@@ -22,29 +22,29 @@ classdef TrainsDeckTest  < matlab.unittest.TestCase
         end
         function DrawTest(obj)
             trainsDeckBlue = TrainsDeck(int8(0),int8(0),int8(13),int8(0),int8(0),int8(0),int8(0),int8(0),int8(0));
-            starting = trainsDeckBlue.init(10,0,2);
+            starting = trainsDeckBlue.init(10,0,2,RandStream('mt19937ar'));
 
             % draw
-            card = trainsDeckBlue.drawCard(TrainCard("unknown"));
+            card = trainsDeckBlue.drawCard(TrainCard("unknown"), RandStream('mt19937ar'));
             obj.verifyEqual(card, TrainCard("blue"));
             obj.verifyEqual(length(trainsDeckBlue.drawPile),2);
             obj.verifyEqual(length(trainsDeckBlue.faceUpCards),0);
             obj.verifyEqual(length(trainsDeckBlue.discardPile),0);
 
             % discard
-            card = trainsDeckBlue.drawCard(TrainCard("unknown"));
-            trainsDeckBlue.discard(TrainCard("blue"));
-            trainsDeckBlue.discard(TrainCard("blue"));
+            card = trainsDeckBlue.drawCard(TrainCard("unknown"), RandStream('mt19937ar'));
+            trainsDeckBlue.discard(TrainCard("blue"), RandStream('mt19937ar'));
+            trainsDeckBlue.discard(TrainCard("blue"), RandStream('mt19937ar'));
             obj.verifyEqual(length(trainsDeckBlue.drawPile),1);
             obj.verifyEqual(length(trainsDeckBlue.discardPile),2);
 
             % reshuffle discard
-            card = trainsDeckBlue.drawCard(TrainCard("unknown"));
+            card = trainsDeckBlue.drawCard(TrainCard("unknown"), RandStream('mt19937ar'));
             obj.verifyEqual(length(trainsDeckBlue.drawPile),2);
             obj.verifyEqual(length(trainsDeckBlue.discardPile),0);
 
             % re-init
-            starting = trainsDeckBlue.init(10,0,2);
+            starting = trainsDeckBlue.init(10,0,2, RandStream('mt19937ar'));
             obj.verifyEqual(length(trainsDeckBlue.drawPile),3);
             obj.verifyEqual(length(trainsDeckBlue.faceUpCards),0);
             obj.verifyEqual(length(trainsDeckBlue.discardPile),0);
